7da94f0293535c308a9b26b776eab2fff257d190
tar04 <taras200195@gmail.com>
2022-04-14 01:01:56 +0300

    added hw4

--- src/services/axiosService.js
+++ src/services/axiosService.js
@@ -1,6 +1,4 @@
 import axios from "axios";
 import baseURL from "../configs/urls";
 
-const axiosService = axios.create({baseURL});
-
-exp
+export const axiosService = axios.create({baseURL});
---
+++ src/services/userService.js
@@ -0,0 +1,12 @@
+import {axiosService} from "./axiosService";
+import {urls} from "../configs";
+
+const userService={
+    getAll:()=>axiosService.get(urls.users).then(value => value.data),
+    getById:(id)=>axiosService.get(`${urls.users}/${id}`).then(value => value.data),
+    getPostsByUsersId:(id)=>axiosService.get(`${urls.users}/${id}${urls.posts}`)
+}
+
+export {
+    userService
+}
